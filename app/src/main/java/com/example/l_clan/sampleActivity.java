70a583d430d155ec08debf5f0b73bc9a4f086370
Omkar Gharge <97081350+IAmOm45@users.noreply.github.com>
2023-03-21 21:55:22 +0530

    omkar second commit

--- app/src/main/java/com/example/l_clan/sampleActivity.java
+++ app/src/main/java/com/example/l_clan/sampleActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class sampleActivity extends AppCompatActivity {
 
-    @Override
+//    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
